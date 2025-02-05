dbeb9a588a85e52fdc7dd206c966db2d67d772e5
nmq <multiloquant@gmail.com>
2025-02-04 22:28:39 -0800

    updates to use internal types

--- mediner/types.py
+++ mediner/types.py
@@ -1,3 +1,4 @@
+import datetime
 from dataclasses import dataclass, asdict, field
 
 
@@ -15,7 +16,7 @@ class Data:
 
 @dataclass
 class Meta:
-    md5: str
+    md5: str = None
 
     def to_dict(self):
         return asdict(self)
@@ -29,9 +30,9 @@ class Meta:
 class SpanValue:
     start: int
     end: int
-    score: float
     text: str
     labels: list[str]
+    score: float = None
 
     def to_dict(self):
         return asdict(self)
@@ -44,7 +45,7 @@ class SpanValue:
 @dataclass
 class EntityResult:
     id: str
-    value: list[SpanValue]
+    value: SpanValue
     from_name: str = "label"
     to_name: str = "text"
     type: str = "labels"
@@ -56,10 +57,7 @@ class EntityResult:
     def from_dict(cls, dictionary):
         return cls(
             id=dictionary['id'],
-            value=[
-                SpanValue.from_dict(span_value)
-                for span_value in dictionary['value']
-            ],
+            value=SpanValue.from_dict(dictionary['value']),
             from_name=dictionary['from_name'],
             to_name=dictionary['to_name'],
             type=dictionary['type'],
@@ -68,9 +66,26 @@ class EntityResult:
 
 @dataclass
 class Annotation:
+    result: list[EntityResult]
+
+    def to_dict(self):
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, dictionary):
+        return cls(
+            result=[
+                EntityResult.from_dict(entity_result)
+                for entity_result in dictionary['result']
+            ]
+        )
+
+
+@dataclass
+class Prediction:
+    result: list[EntityResult]
     model_version: str
     score: float
-    result: list[EntityResult]
 
     def to_dict(self):
         return asdict(self)
@@ -88,27 +103,35 @@ class Annotation:
 
 
 @dataclass
-class Prediction(Annotation):
-    pass
-
-
-@dataclass
 class Task:
     data: Data
     meta: Meta
+    updated_at: datetime.datetime = None
     predictions: list[Prediction] = field(default_factory=list)
-    #annotations: list[Annotation] = field(default_factory=list)
+    annotations: list[Annotation] = field(default_factory=list)
 
     def to_dict(self):
-        return asdict(self)
+        dictionary = asdict(self)
+        if self.updated_at is not None:
+            dictionary['updated_at'] = self.updated_at.isoformat().replace("+00:00", "Z")
+        return dictionary
 
     @classmethod
     def from_dict(cls, dictionary: dict):
+        updated_at = None
+        if 'updated_at' in dictionary:
+            updated_at = datetime.datetime.fromisoformat(dictionary['updated_at'])
+
         return cls(
             data=Data.from_dict(dictionary['data']),
             meta=Meta.from_dict(dictionary['meta']),
+            updated_at=updated_at,
             predictions=[
                 Prediction.from_dict(prediction)
                 for prediction in dictionary.get("predictions", [])
+            ],
+            annotations=[
+                Annotation.from_dict(annotation)
+                for annotation in dictionary.get("annotations", [])
             ]
         )
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,6 @@
 from mediner import transformations
+from mediner import types
+from spacy.tokens import DocBin
 
 
 def test_text_to_md5():
@@ -21,14 +23,22 @@ def test_text_to_md5_empty():
     assert output and isinstance(output, str)
 
 
-def test_files_to_annotations(mock_label_studio_export_json_filename):
+def test_files_to_tasks(mock_label_studio_export_json_filename):
+    """Test making tasks from json input format
+
+    :return None:
+    :raises AssertionError:
+    """
+    tasks = transformations.files_to_tasks([mock_label_studio_export_json_filename])
+    assert tasks and all([isinstance(task, types.Task) for task in tasks])
+
+
+def test_tasks_to_docbin(mock_label_studio_export_json_filename):
     """Test making a md5 hash string from an empty input
 
     :return None:
     :raises AssertionError:
     """
-    annotations = transformations.files_to_annotations([mock_label_studio_export_json_filename])
-    assert annotations
-
-
-#def test_annotations_to_docbin()
+    tasks = transformations.files_to_tasks([mock_label_studio_export_json_filename])
+    docbin = transformations.tasks_to_docbin(tasks)
+    assert docbin and isinstance(docbin, DocBin) and len(docbin) == len(tasks)
