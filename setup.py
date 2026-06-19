0d131d4f571817effe0a1cc333cac204097e0f6a
nmq <multiloquant@gmail.com>
2026-06-18 22:08:52 -0700

    tag bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_requirements(filename: str):
 
 setuptools.setup(
     name="mediner",
-    version='0.0.3',
+    version='0.0.4',
     description="Medical NER",
     long_description="Named Entity Recognition for the VA Hospital",
     author="Nathan McCoy",
