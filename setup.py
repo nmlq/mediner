46dbb57c80d40cbcc9665358430fcc99a7bc564c
nmq <multiloquant@gmail.com>
2026-08-12 12:00:36 -0700

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_requirements(filename: str):
 
 setuptools.setup(
     name="mediner",
-    version='0.0.6',
+    version='0.0.7',
     description="Medical NER",
     long_description="Named Entity Recognition for the VA Hospital",
     author="Nathan McCoy",
